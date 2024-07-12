2b8e952f6042d21ec68ec35d9198bce43dcaca10
TranVinhTanDat <tandat27012002.td@gmail.com>
2024-07-12 20:05:29 +0700

    init project Api

---
+++ src/main/java/com/example/shoppecommerce/ShoppecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.shoppecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shoppecommerce/ShoppecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
