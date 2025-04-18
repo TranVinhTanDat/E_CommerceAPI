fbd9662f5267bb32ad6ae00a02c79d770597c25f
TranVinhTanDat <tandat27012002.td@gmail.com>
2025-04-18 20:22:43 +0700

    update

--- src/main/java/com/example/shoppecommerce/Config/WebSocketConfig.java
+++ src/main/java/com/example/shoppecommerce/Config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:3000")
+                .setAllowedOriginPatterns("http://localhost:3000", "https://e-commerce-fe-seven-snowy.vercel.app")
                 .withSockJS();
     }
 }
